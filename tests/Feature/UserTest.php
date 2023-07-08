b66f15f8300964e30c9b93ea47ce2397201c6af6
VuacheslavMironov <vuacheslav.mir@gmail.com>
2023-07-09 01:55:35 +0700

    Сделал тесты на поик профиля

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,13 +2,12 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 use App\DTO\User\CreateUserDTO;
+use App\DTO\User\SearchUserByIdDTO;
+use App\DTO\User\SearchUserByEmailDTO;
 use App\Domain\Service\UserService;
-
 
 
 class UserTest extends TestCase
@@ -25,6 +24,7 @@ class UserTest extends TestCase
         $dtoCreateUser->Email = "howeda1586@nasskar.com";
         $dtoCreateUser->Phone = null;
         $dtoCreateUser->Password = "babka123";
+
         // Проверка CreateUserDTO
         $this->assertEquals($dtoCreateUser->FirstName, "Игорь");
         $this->assertEquals($dtoCreateUser->LastName, "Мохов");
@@ -33,6 +33,7 @@ class UserTest extends TestCase
         $this->assertEquals($dtoCreateUser->Password, "babka123");
         $userService = new UserService();
         $user = $userService->CreateUserService($dtoCreateUser);
+
         // Проверка CreateUserService
         $this->assertNotNull($user);
         $this->assertEquals($user->FirstName, "Игорь");
@@ -40,5 +41,106 @@ class UserTest extends TestCase
         $this->assertEquals($user->Email, "howeda1586@nasskar.com");
         $this->assertEquals($user->Phone, null);
         $this->assertEquals($user->Password, "babka123");
+    }
+
+    /**
+     * A basic feature test show_by_id.
+     */
+    public function test_show_by_id(): void
+    {
+        $dtoCreateUser = new CreateUserDTO();
+        $dtoCreateUser->FirstName = "Игорь";
+        $dtoCreateUser->LastName = "Мохов";
+        $dtoCreateUser->Email = "howeda1586@nasskar.com";
+        $dtoCreateUser->Phone = null;
+        $dtoCreateUser->Password = "babka123";
+
+        // Проверка CreateUserDTO
+        $this->assertEquals($dtoCreateUser->FirstName, "Игорь");
+        $this->assertEquals($dtoCreateUser->LastName, "Мохов");
+        $this->assertEquals($dtoCreateUser->Email, "howeda1586@nasskar.com");
+        $this->assertEquals($dtoCreateUser->Phone, null);
+        $this->assertEquals($dtoCreateUser->Password, "babka123");
+
+        // Вызов сервиса UserService
+        $userService = new UserService();
+        $user = $userService->CreateUserService($dtoCreateUser);
+
+        // Проверка CreateUserService
+        $this->assertNotNull($user);
+        $this->assertEquals($user->FirstName, "Игорь");
+        $this->assertEquals($user->LastName, "Мохов");
+        $this->assertEquals($user->Email, "howeda1586@nasskar.com");
+        $this->assertEquals($user->Phone, null);
+        $this->assertEquals($user->Password, "babka123");
+
+        $user_id = json_decode(collect($user), true)['id'];
+
+        $dtoShowByIdUser = new SearchUserByIdDTO();
+        $dtoShowByIdUser->Id = $user_id;
+
+        // Проверка SearchUserByIdDTO
+        $this->assertEquals($dtoShowByIdUser->Id, $user_id);
+
+        // Вызов сервиса UserService
+        $userService = new UserService();
+        $user = $userService->ShowUserByIdService($dtoShowByIdUser);
+        // Проверка CreateUserService
+        $this->assertNotNull($user);
+        $this->assertEquals(rtrim($user->FirstName), "Игорь");
+        $this->assertEquals(rtrim($user->LastName), "Мохов");
+        $this->assertEquals($user->Email, "howeda1586@nasskar.com");
+        $this->assertEquals($user->Phone, null);
+    }
+
+    /**
+     * A basic feature test show_by_email.
+     */
+    public function test_show_by_email(): void
+    {
+        $dtoCreateUser = new CreateUserDTO();
+        $dtoCreateUser->FirstName = "Игорь";
+        $dtoCreateUser->LastName = "Мохов";
+        $dtoCreateUser->Email = "howeda1586@nasskar.com";
+        $dtoCreateUser->Phone = null;
+        $dtoCreateUser->Password = "babka123";
+
+        // Проверка CreateUserDTO
+        $this->assertEquals($dtoCreateUser->FirstName, "Игорь");
+        $this->assertEquals($dtoCreateUser->LastName, "Мохов");
+        $this->assertEquals($dtoCreateUser->Email, "howeda1586@nasskar.com");
+        $this->assertEquals($dtoCreateUser->Phone, null);
+        $this->assertEquals($dtoCreateUser->Password, "babka123");
+
+        // Вызов сервиса UserService
+        $userService = new UserService();
+        $user = $userService->CreateUserService($dtoCreateUser);
+
+        // Проверка CreateUserService
+        $this->assertNotNull($user);
+        $this->assertEquals($user->FirstName, "Игорь");
+        $this->assertEquals($user->LastName, "Мохов");
+        $this->assertEquals($user->Email, "howeda1586@nasskar.com");
+        $this->assertEquals($user->Phone, null);
+        $this->assertEquals($user->Password, "babka123");
+
+        $user_email = json_decode(collect($user), true)['Email'];
+
+        $dtoShowByEmailUser = new SearchUserByEmailDTO();
+        $dtoShowByEmailUser->Email = $user_email;
+
+        // Проверка SearchUserByIdDTO
+        $this->assertEquals($dtoShowByEmailUser->Email, $user_email);
+
+        // Вызов сервиса UserService
+        $userService = new UserService();
+        $user = json_decode($userService->ShowUserByEmailService($dtoShowByEmailUser)[0], true);
+        
+        // Проверка CreateUserService
+        $this->assertNotNull($user);
+        $this->assertEquals(rtrim($user['FirstName']), "Игорь");
+        $this->assertEquals(rtrim($user['LastName']), "Мохов");
+        $this->assertEquals($user['Email'], "howeda1586@nasskar.com");
+        $this->assertEquals($user['Phone'], null);
     }
 }
